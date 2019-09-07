794128dcb1c0086259d7dd7f32d0beebea8ba0cf
Yura <yura19980412@Gmail.com>
2019-09-07 21:27:59 +0300

    Test omni

--- Omnifood website/js/script.js
+++ Omnifood website/js/script.js
@@ -1,4 +1,7 @@
  $(document).ready(function() {
+
+   /*For the sticky navigation*/
+
    $('.js--section-features').waypoint(function(direction) {
      if (direction == "down"){
        $('nav').addClass('sticky');
@@ -6,7 +9,13 @@
        $('nav').removeClass('sticky');
      }
      },{
-       offset:'60px'
+       offset:'60px;'
+   });
+
+   /*Scroll on buttons*/
+   $('.js--scroll-to-plans').click(function(){
+     $('html, body').animate({scrollTop: $('.js--section-plans').offset().top},1000); 
+
    });
     
 });
